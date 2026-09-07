6eb6ced767eeb103ee6370613ec292dbc22c02bd
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-09-07 12:08:46 -0300

    cambio iniciales

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const series = require("./data/series.json");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  console.log(`la APP esta escuchando en el puerto ${PORT}...`);
+});
+
+//////////todos
+
+app.get("/series", (req, response) => {
+  response.status(200).json(series);
+});
+
+////// filtrar por id
+
+app.get("/series/:id", (req, response) => {
+  if (isNaN(req.params.id)) {
+    response.status(400).json({ msj: "El id debe ser numerico" });
+    return;
+  }
+  const id = Number(req.params.id);
+  const serieEncontrada = series.find((serie) => serie.id === id);
+
+  if (serieEncontrada) {
+    response.status(200).json(serieEncontrada);
+  } else {
+    response.status(404).json({ msg: `el id ${id} no se encuentra.` });
+  }
+});
+
+///// eliminar por id
+
+app.delete("/series/:id", (req, response) => {
+  if (isNaN(req.params.id)) {
+    response.status(400).json({ msj: "El id debe ser numerico" });
+    return;
+  }
+  const id = Number(req.params.id);
+  const indice = series.findIndex((serie) => serie.id === id);
+
+  if (indice === -1) {
+    response.status(404).json({ msg: `el id ${id} no se encuentra!.` });
+    return;
+  }
+
+  const serieBorrada = series.splice(indice, 1);
+
+  response.status(200).json({ msg: "serie eliminada", serie: serieBorrada[0] });
+});
+
+/// crear una nueva serie
+
+app.post("/series", (req, response) => {
+  const body = req.body;
+  response.status(201).json(body);
+});
